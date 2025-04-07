f3fbf79b94c0c5ed94a0f9f94862bdc886fd2a02
seungwonHong <nonamed814@gmail.com>
2025-04-08 01:19:45 +0900

    fic

--- src/app/resultPage/page.js
+++ src/app/resultPage/page.js
@@ -2,6 +2,8 @@ import Result from "@/components/Result";
 import dbConnect from "@/lib/db/dbConnect";
 import ShortLink from "@/lib/db/models/ShortLink";
 
+export const dynamic = "force-dynamic";
+
 export default async function resultPage() {
   await dbConnect();
   const rawLinks = await ShortLink.find().lean();
