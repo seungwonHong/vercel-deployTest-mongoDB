b6e4c4ddc654449c61f6473e00a5183b8a4b22d7
seungwonHong <nonamed814@gmail.com>
2025-04-08 01:05:30 +0900

    fix

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Result = ({response}) => {
-    return (
-      <div>
-        {response.map((value) => (
-          <div key={value._id}>
-            <p>{value.title}</p>
-            <p>{value.url}</p>
-          </div>
-        ))}
-      </div>
-    );
+console.log("shortlink connect");
+const Result = ({ response }) => {
+  return (
+    <div>
+      {response.map((value) => (
+        <div key={value._id}>
+          <p>{value.title}</p>
+          <p>{value.url}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Result;
